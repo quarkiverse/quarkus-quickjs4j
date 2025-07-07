2940a6c22ff6cc632a7e482a3c299b2529a2c7f8
Eric Wittmann <eric.wittmann@gmail.com>
2025-07-07 13:47:30 -0400

    Get native build/test working properly

--- runtime/src/main/java/io/quarkiverse/quickjs4j/util/ScriptInterfaceUtils.java
+++ runtime/src/main/java/io/quarkiverse/quickjs4j/util/ScriptInterfaceUtils.java
@@ -13,30 +13,39 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class ScriptInterfaceUtils {
 
+    private static final Logger LOGGER = Logger.getLogger(ScriptInterfaceUtils.class.getName());
+
     public static String loadScriptLibrary(String scriptLocation) {
+        LOGGER.fine("Loading script library from: " + scriptLocation);
+
         Path path = Paths.get(scriptLocation);
         if (!path.isAbsolute()) {
             String workingDir = System.getProperty("user.dir");
             path = Paths.get(workingDir).resolve(path).normalize();
         }
         if (Files.exists(path)) {
+            LOGGER.fine("Script library file found at: " + path.toAbsolutePath());
             return loadScriptLibrary(path);
         }
 
         URL resourceUrl = Thread.currentThread().getContextClassLoader().getResource(scriptLocation);
         if (resourceUrl != null) {
+            LOGGER.fine("Script library resource [1] found at: " + resourceUrl.toExternalForm());
             return loadScriptLibrary(resourceUrl);
         }
 
         resourceUrl = ScriptInterfaceUtils.class.getClassLoader().getResource(scriptLocation);
         if (resourceUrl != null) {
+            LOGGER.fine("Script library resource [2] found at: " + resourceUrl.toExternalForm());
             return loadScriptLibrary(resourceUrl);
         }
 
+        LOGGER.warning("Script library [" + scriptLocation + "] not found!!");
         return null;
     }
 
